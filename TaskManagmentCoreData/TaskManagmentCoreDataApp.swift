f090e97524f3ba6adcd1631f70b4226f48549698
Max Chesnikov <m.chesnikov@app-smart.ru>
2022-01-15 18:08:04 +0300

    Initial Commit

---
+++ TaskManagmentCoreData/TaskManagmentCoreDataApp.swift
@@ -0,0 +1,20 @@
+//
+//  TaskManagmentCoreDataApp.swift
+//  TaskManagmentCoreData
+//
+//  Created by Максим Чесников on 15.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagmentCoreDataApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
