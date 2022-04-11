7bb677c72b3bbb8fd81c12d7e42ef0ed33b32971
Max Chesnikov <m.chesnikov@app-smart.ru>
2022-04-11 03:44:39 +0300

    Add logo Fix migration

--- TaskManagmentCoreData/Persistence.swift
+++ TaskManagmentCoreData/Persistence.swift
@@ -33,6 +33,8 @@ struct PersistenceController {
 
     init(inMemory: Bool = false) {
         container = NSPersistentCloudKitContainer(name: "TaskManagmentCoreData")
+        let description = container.persistentStoreDescriptions.first
+        description?.setOption(true as NSNumber, forKey: NSPersistentHistoryTrackingKey)
         if inMemory {
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
         }
